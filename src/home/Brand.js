97403e1f4cd6618301721c146a161316528d452b
Abolfazl <abolfazlm181920@gmail.com>
2022-05-04 21:18:00 +0430

    link items

--- src/home/Brand.js
+++ src/home/Brand.js
@@ -1,7 +1,9 @@
+import { Link } from 'react-router-dom'
+
 const Brand = () => {
     return (
         <div className="text-xl font-semibold text-gray-700">
-            <a className="text-2xl font-bold text-gray-800 dark:text-white lg:text-3xl hover:text-blue-500 dark:hover:text-gray-300" href="/">FoodDelivery</a>
+            <Link to='/' className="text-2xl font-bold text-gray-800 dark:text-white lg:text-3xl hover:text-blue-500 dark:hover:text-gray-300" href="/">FoodDelivery</Link>
         </div>
     )
 }
--- src/home/Items.js
+++ src/home/Items.js
@@ -1,7 +1,9 @@
+import { Link } from 'react-router-dom'
+
 const Items = () => {
     return (
         <div className="-mx-4 lg:flex lg:items-center">
-            <a href="/" className="block mx-4 mt-2 text-sm text-gray-700 capitalize lg:mt-0 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400">Test1</a>
+            <Link to="/test1" className="block mx-4 mt-2 text-sm text-gray-700 capitalize lg:mt-0 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400">Test1</Link>
         </div>
     )
 }
