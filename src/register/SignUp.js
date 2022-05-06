50458acb3b0d975cedd2e7f8a81c2ad9a7e58a6c
Abolfazl <abolfazlm181920@gmail.com>
2022-05-06 16:12:28 +0430

    create states for inputs

--- src/register/SignUp.js
+++ src/register/SignUp.js
@@ -1,4 +1,14 @@
+import { useState } from 'react'
+
 const SignUp = () => {
+
+    const [ name, setName ] = useState([])
+    const [ password, setPassword ] = useState([])
+
+    const handleForm = (event) => {
+        event.preventDefault()
+    }
+
     return (
         <>
             <div class="w-full mt-10 mobile:w-80 max-w-sm p-6 m-auto bg-white rounded-md shadow-md dark:bg-gray-800">
@@ -8,6 +18,7 @@ const SignUp = () => {
                 <div>
                     <label for="username" class="block text-sm text-gray-800 dark:text-gray-200">Username</label>
                     <input type="text"
+                        value={name}
                         class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
                 </div>
 
@@ -17,11 +28,13 @@ const SignUp = () => {
                     </div>
 
                     <input type="password"
+                        value={password}
                         class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
                 </div>
 
                 <div class="mt-6">
                     <button
+                        onClick={handleForm}
                         class="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
                         SignUp
                     </button>
