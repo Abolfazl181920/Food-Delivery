e6d226daaefbe9991a61f1121baeae40f208b52a
Abolfazl <abolfazlm181920@gmail.com>
2022-05-06 16:28:11 +0430

    add some funcs to SignUp.js

--- src/register/SignUp.js
+++ src/register/SignUp.js
@@ -30,7 +30,7 @@ const SignUp = () => {
                     <input type="text"
                         onChange={handleName}
                         value={name}
-                        placeholder='ex: Abolfazl'
+                        placeholder='ex: Abolfazl' 
                         className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
                 </div>
 
