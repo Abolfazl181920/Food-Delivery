f4b9a4ad423c7a89c4890175417193e37d566cf4
Abolfazl <abolfazlm181920@gmail.com>
2022-05-12 20:00:55 +0430

    nav to foods page if all infos are true

--- src/register/SignIn.js
+++ src/register/SignIn.js
@@ -30,10 +30,12 @@ const SignIn = () => {
                 </div>
 
                 <div className="mt-6">
-                    <button
-                        className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
-                        Login
-                    </button>
+                    <Link to='/foods'>
+                        <button
+                            className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
+                            Login
+                        </button>
+                    </Link>
                 </div>
             </form>
 
