e24f0e4882a9dacce10bfcda20efeb486cc84e99
Abolfazl <abolfazlm181920@gmail.com>
2022-05-27 16:24:31 +0430

    fetch
    change the address of fetch method in Foods.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import App from 'App'
 import FoodsPage from 'pages/FoodsPage'
+// import RecipePage from 'pages/RecipePage'
 import SignUp from './register/SignUp'
 import SignIn from './register/SignIn'
 import Error404 from './404/Error404'
@@ -17,6 +18,7 @@ root.render(
     <Routes>
       <Route path='/' element={ <App /> } />
       <Route path='foods' element={ <FoodsPage /> } />
+      {/* <Route path='recipe' element={ <RecipePage /> } /> */}
       <Route path='registration/signup' element={ <SignUp /> } />
       <Route path='registration/signin' element={ <SignIn /> } />
       <Route path='*' element={ <Error404 /> } />
---
+++ src/pages/RecipePage.js
@@ -0,0 +1,9 @@
+const RecipePage = () => {
+    return (
+        <div>
+            This is Recipe page
+        </div>
+    )
+}
+
+export default RecipePage
