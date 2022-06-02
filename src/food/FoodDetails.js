4dbf0043a83157ef364afc50147544708a4a68e7
Abolfazl <abolfazlm181920@gmail.com>
2022-06-02 19:17:47 +0430

    dynamic routes: done it!

--- src/food/FoodDetails.js
+++ src/food/FoodDetails.js
@@ -1,12 +1,27 @@
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 const FoodDetails = () => {
 
-    const foodId = useParams()
+    const [ data, setData ] = useState()
+    const { foodId } = useParams()
+
+    useEffect(() => {
+        async function fetchData() {
+            const fetchData = await fetch (
+                `http://localhost:3003/foods/${foodId}`
+            )
+            const response = await fetchData.json()
+            setData(response)
+        }
+        fetchData()
+
+    }, [ data ])
 
     return (
         <div>
             This is FoodDetails for each foods ... : {JSON.stringify(foodId)}
+            This is {JSON.stringify(data)}
         </div>
     )
 }
