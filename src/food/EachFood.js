1d833188150168b83d6c61009a8c0a28a6453ebc
Abolfazl <abolfazlm181920@gmail.com>
2022-06-03 22:46:49 +0430

    better code: add better code for adding foods

--- src/food/EachFood.js
+++ src/food/EachFood.js
@@ -36,7 +36,9 @@ const EachFood = ({ name, source, price }) => {
                     type="button"
                     className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-xl text-sm px-4 py-2 text-center inline-flex items-center mr-2">
                 <Pluse />
-                    Buy First
+                    {
+                        count === 0 ? 'Buy First' : count
+                    }
                 </button>
                 <button
                     onClick={removeFood}
@@ -45,9 +47,6 @@ const EachFood = ({ name, source, price }) => {
                 <Minuse />
                     Cancel
                 </button>
-            </div>
-            <div className="flex justify-center">
-                <span className="h-5 w-5 items-center text-center rounded mt-3 bg-yellow-500"> {count} </span>
             </div>
         </div>
     )
