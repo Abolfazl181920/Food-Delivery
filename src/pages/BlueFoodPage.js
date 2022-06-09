97c8cff3909b4a9c857e830a430e8509177b91d6
Abolfazl <abolfazlm181920@gmail.com>
2022-06-09 17:32:22 +0430

    create random number

--- src/pages/BlueFoodPage.js
+++ src/pages/BlueFoodPage.js
@@ -1,7 +1,23 @@
+import { useState } from 'react'
+
 const BlueFoodPage = () => {
+
+    const [ randomNumber, setRandomNumber ] = useState(0)
+
+    const randomNumberRange = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1)) + min
+    }
+
+    const handleClick = () => {
+        setRandomNumber(randomNumberRange(200, 700))
+    }
+
     return (
         <div>
             This is BlueFoodPage ...
+            number is : {randomNumber}
+            <br/>
+            <button onClick={handleClick}>click</button>
         </div>
     )
 }
