91e3d4b7cf07df4c11c65f4ef265f7547084066a
Abolfazl <abolfazlm181920@gmail.com>
2022-06-10 18:33:42 +0430

    icons

--- src/icons/icons.js
+++ src/icons/icons.js
@@ -15,3 +15,11 @@ export const Euro = () => (
     <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.736 6.979C9.208 6.193 9.696 6 10 6c.304 0 .792.193 1.264.979a1 1 0 001.715-1.029C12.279 4.784 11.232 4 10 4s-2.279.784-2.979 1.95c-.285.475-.507 1-.67 1.55H6a1 1 0 000 2h.013a9.358 9.358 0 000 1H6a1 1 0 100 2h.351c.163.55.385 1.075.67 1.55C7.721 15.216 8.768 16 10 16s2.279-.784 2.979-1.95a1 1 0 10-1.715-1.029c-.472.786-.96.979-1.264.979-.304 0-.792-.193-1.264-.979a4.265 4.265 0 01-.264-.521H10a1 1 0 100-2H8.017a7.36 7.36 0 010-1H10a1 1 0 100-2H8.472c.08-.185.167-.36.264-.521z" clipRule="evenodd" />
     </svg>
 )
+
+export const Eye = () => (
+    <svg className="-ml-0.5 mr-2 h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 12a2 2 0 100-4 2 2 0 000 4z"></path><path fillRule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clipRule="evenodd"></path></svg>
+)
+
+export const Info = () => (
+    <svg className="mr-2 w-5 h-5 text-blue-700 dark:text-blue-800" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd"></path></svg>
+)
--- src/pages/BlueFoodPage.js
+++ src/pages/BlueFoodPage.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 
+import { Eye, Info } from '../icons/icons'
 import KILLILLU from '../assests/KILLILLU.png'
 import BlueFoodImg from '../assests/bluefood.png'
 
@@ -31,7 +32,24 @@ const BlueFoodPage = () => {
                 <button onClick={handleClick} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Try to get Off</button>
                 <span className="bg-yellow-500 h-10 w-10 rounded-lg text-center"> {randomNumber} </span>
             </div>
-        </>
+
+            <div className="flex items-center">
+                <Info />
+                <h3 className="text-lg font-medium text-blue-700 dark:text-blue-800">This is a info alert</h3>
+            </div>
+                <div className="mt-2 mb-4 text-sm text-blue-700 dark:text-blue-800">
+                More info about this info alert goes here. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.
+                </div>
+                <div className="flex">
+                    <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xs px-3 py-1.5 mr-2 text-center inline-flex items-center dark:bg-blue-800 dark:hover:bg-blue-900">
+                         <Eye />
+                    View more
+                    </button>
+                    <button className="text-blue-700 bg-transparent border border-blue-700 hover:bg-blue-800 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xs px-3 py-1.5 text-center dark:border-blue-800 dark:text-blue-800 dark:hover:text-white" data-dismiss-target="#alert-additional-content-1" aria-label="Close">
+                    Dismiss
+                    </button>
+                </div>
+            </>
     )
 }
 
