119dd5041d992f465a40f883e8c9b591c3ff1ce8
Abolfazl <abolfazlm181920@gmail.com>
2022-06-10 20:10:23 +0430

    create Price Box and styled it

--- src/food/PriceBox.js
+++ src/food/PriceBox.js
@@ -1,8 +1,23 @@
+import { Link } from 'react-router-dom'
+import { Eye } from '../icons/icons'
+
 const PriceBox = ({ finalPrice }) => {
     return (
-        <div>
-            <span> finalPrice is: {finalPrice} </span>
-        </div>
+        <>
+            <div className="p-3 mb-3 w-52 bg-blue-100 rounded-2xl dark:bg-blue-200" role="alert">
+                <div className="mt-2 mb-4 text-sm text-blue-700 dark:text-blue-800">
+                    <span> final price is: {finalPrice} </span>
+                </div>
+                <div className="flex">
+                    <Link to="">
+                        <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xs px-3 py-1.5 mr-2 text-center inline-flex items-center dark:bg-blue-800 dark:hover:bg-blue-900">
+                            <Eye />
+                            Go To Buy
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        </>
     )
 }
 
